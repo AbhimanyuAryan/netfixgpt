b78f9203ebe61417e96025d070e79c2ec953154f
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-06-13 20:36:56 +0100

    restructuring the repo

--- src/netfixgpt/client.py
+++ src/netfixgpt/client.py
@@ -1,3 +1,26 @@
 import pathlib
 import logging
 import click
+import os
+
+@click.group()
+def app():
+    intro = """
+        _______  _________________________________._______  ________________________________
+        \      \ \_   _____/\__    ___/\_   _____/|   \   \/  /  _____/\______   \__    ___/
+        /   |   \ |    __)_   |    |    |    __)  |   |\     /   \  ___ |     ___/ |    |   
+        /    |    \|        \  |    |    |     \   |   |/     \    \_\  \|    |     |    |   
+        \____|__  /_______  /  |____|    \___  /   |___/___/\  \______  /|____|     |____|   
+                \/        \/                 \/              \_/      \/                     
+    """
+    click.echo(
+        click.style(
+            intro, fg="bright_blue"
+        )
+    )
+
+def main():
+    app(standalone_mode=True)
+
+if __name__ == "__main__":
+    main()
